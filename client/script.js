4910ed0c55a27531423db77b63e9b0796f9d48a0
Siddhartha Roy <sid86harth@gmail.com>
2023-03-26 14:50:28 +0530

    env variable

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch response from server
-  const response = await fetch('http://localhost:8000', {
+  const response = await fetch(process.env.SERVER_URL, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
--- server/server.js
+++ server/server.js
@@ -43,6 +43,6 @@ app.post('/', async (req, res) => {
     }
 });
 
-app.listen(8000, () => {
-    console.log('Server running on port http://localhost:8000');
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
 });
